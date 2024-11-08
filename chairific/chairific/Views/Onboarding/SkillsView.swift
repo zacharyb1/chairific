32b2d1441f97209c02c054169180afa039d82eee
Ivan <ivan.semeniuk2004@gmail.com>
2024-11-08 19:20:14 +0200

    Starting implementation of the SkillsVies

---
+++ chairific/chairific/Views/Onboarding/SkillsView.swift
@@ -0,0 +1,18 @@
+//
+//  SkillsView.swift
+//  chairific
+//
+//  Created by Ivan Semeniuk on 08/11/2024.
+//
+
+import SwiftUI
+
+struct SkillsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkillsView()
+}
