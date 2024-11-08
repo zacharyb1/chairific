6752a7a7ad655cc23863a657a001d6e9feec6244
Ivan <ivan.semeniuk2004@gmail.com>
2024-11-08 17:57:42 +0200

    Added Firebase packages, changed deployment version

--- chairific/chairific/chairificApp.swift
+++ chairific/chairific/chairificApp.swift
@@ -6,9 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseCore
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        FirebaseConfiguration.shared.setLoggerLevel(.debug)
+        return true
+    }
+}
+
 
 @main
 struct chairificApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
