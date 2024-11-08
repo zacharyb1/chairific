3beb92931c2274386fba184d012183f477b0392a
Ivan <ivan.semeniuk2004@gmail.com>
2024-11-08 17:51:32 +0200

    Chairific project

---
+++ chairific/chairific/chairificApp.swift
@@ -0,0 +1,17 @@
+//
+//  chairificApp.swift
+//  chairific
+//
+//  Created by Ivan Semeniuk on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct chairificApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chairific/chairificTests/chairificTests.swift
@@ -0,0 +1,17 @@
+//
+//  chairificTests.swift
+//  chairificTests
+//
+//  Created by Ivan Semeniuk on 08/11/2024.
+//
+
+import Testing
+@testable import chairific
+
+struct chairificTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
