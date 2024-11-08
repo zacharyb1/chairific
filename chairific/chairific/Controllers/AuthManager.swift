c2905aaef4b4dc096af5ac8211b315a9f24942f5
Ivan <ivan.semeniuk2004@gmail.com>
2024-11-08 18:09:17 +0200

    created controller for authorization AuthManager

---
+++ chairific/chairific/Controllers/AuthManager.swift
@@ -0,0 +1,47 @@
+//
+//  AuthManager.swift
+//  chairific
+//
+//  Created by Ivan Semeniuk on 08/11/2024.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthManager{
+    static let shared = AuthManager()
+
+    private init() {}
+    
+    func getCurrentUser() -> User? {
+        return Auth.auth().currentUser
+    }
+    
+    func signOutUser(completion: @escaping (Result<Void, Error>) -> Void) {
+        do {
+            try Auth.auth().signOut()
+            completion(.success(()))
+        } catch let signOutError as NSError {
+            completion(.failure(signOutError))
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping (Result<AuthDataResult, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let authResult = authResult {
+                completion(.success(authResult))
+            }
+        }
+    }
+    func signInUser(email: String, password: String, completion: @escaping (Result<AuthDataResult, Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let authResult = authResult {
+                completion(.success(authResult))
+            }
+        }
+    }
+}
